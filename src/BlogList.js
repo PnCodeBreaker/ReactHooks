f9eae393abc6bce509a0b0d43f52283e7c156058
PnCodeBreaker <prithwishnp.n@gmail.com>
2021-02-28 13:34:14 +0530

    React-Router

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const BlogList = ({ blogs, title }) => {
   
@@ -7,8 +8,11 @@ const BlogList = ({ blogs, title }) => {
             <h2>{ title }</h2>
             { blogs.map( (blog) => (
             <div className="blog-preview" key={blog.id}>
+                <Link to={`/blogs/${blog.id}`}>
                 <h2>{blog.title}</h2>
                 <p>written by {blog.author}</p>
+                </Link>
+               
             </div>
        ))}
         </div>
